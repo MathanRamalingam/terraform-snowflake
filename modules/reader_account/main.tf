98a7aed895250fd735b686836a0ec8ddcc245f33
MathanRamalingam <69763933+MathanRamalingam@users.noreply.github.com>
2026-06-09 22:16:17 +0200

    Update main.tf

--- modules/reader_account/main.tf
+++ modules/reader_account/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflakedb/snowflake"
+      version = "~> 1.0"
+    }
+  }
+}
+
+resource "snowflake_managed_account" "reader" {
+  name           = var.account_name
+  admin_name     = var.admin_name
+  admin_password = var.admin_password
+  type           = "READER"
+  comment        = var.comment
+}
+
 resource "snowflake_managed_account" "reader" {
     name = var.account_name
     admin_name = var.admin_name
