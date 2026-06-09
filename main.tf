2f7805b658ad581f2eab029a71e94cf4842fe105
MathanRamalingam <69763933+MathanRamalingam@users.noreply.github.com>
2026-06-09 23:11:37 +0200

    Refactor main.tf for improved organization

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 provider "snowflake" {
   organization_name = var.snowflake_organization
   account_name      = var.snowflake_account
-  user              = var.snowflake_username
+  user              = var.snowflake_user
   password          = var.snowflake_password
   role              = "ACCOUNTADMIN"
 }
