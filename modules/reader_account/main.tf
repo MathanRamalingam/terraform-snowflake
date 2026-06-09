8487f61203093c7adbe16d630759ae5160cef9de
MathanRamalingam <69763933+MathanRamalingam@users.noreply.github.com>
2026-06-09 22:19:27 +0200

    Format Terraform resource block for consistency

--- modules/reader_account/main.tf
+++ modules/reader_account/main.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflakedb/snowflake"
+      version = "~> 1.0"
+    }
+  }
+}
+
 resource "snowflake_managed_account" "reader" {
-    name = var.account_name
-    admin_name = var.admin_name
-    admin_password = var.admin_password
-    type = "READER"
-    comment = "var.comment"
+  name           = var.account_name
+  admin_name     = var.admin_name
+  admin_password = var.admin_password
+  type           = "READER"
+  comment        = var.comment
 }
